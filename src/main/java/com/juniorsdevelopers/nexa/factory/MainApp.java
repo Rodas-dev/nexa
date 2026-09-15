fdf5c422927e508693ddcee305ef542fd2335d54
Rodas-dev <rodasvldz@gmail.com>
2026-09-15 12:36:17 -0600

    estructura del proyecto

---
+++ src/main/java/com/juniorsdevelopers/nexa/factory/MainApp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main.java.com.juniorsdevelopers.nexa.factory;
+
+/**
+ *
+ * @author DELL
+ */
+public class MainApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
