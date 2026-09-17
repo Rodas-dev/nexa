995793f7f4d90312db0840a23ab63aa0ed8be962
Jurgen-Paiz <chavezjosiel832@gmail.com>
2026-09-17 16:58:43 -0600

    Delete Credentials class with database credentials
    Removed Credentials class containing database connection details.

--- src/main/java/com/juniorsdevelopers/nexa/factory/config/Credentials.java
+++ src/main/java/com/juniorsdevelopers/nexa/factory/config/Credentials.java
@@ -1,10 +1,1 @@
 
-package com.juniorsdevelopers.nexa.factory.config;
-
-
-public class Credentials {
-    public static final String URL_DATA_BASE = "jdbc:mysql://localhost:3306/sistema_produccion_in4bv";
-    public static final String USER_DB = "IN4BV";
-    public static final String PASS_DB = "%IndiVA4";
-}
-
